38fc97d004504b85757510892096a43e7a3cb8d7
archd07 <ahmed.rachid665@gmail.com>
2025-10-27 21:02:31 +0100

    hibernate config

--- src/main/java/dao/User.java
+++ src/main/java/dao/User.java
@@ -1,14 +1,27 @@
 package dao;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
-    private int id;
+    @Id
+    private long id;
+    @Column(nullable = false)
     private String name;
+    @Column(nullable = false)
     private String email;
-    private String password;
-    private String role;
-    private String city;
-    private boolean verified;
+    @Column(nullable = false)
     private String phone;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String role;
+    @Column(nullable = false)
+    private String city;
+    @Column(nullable = false)
+    private boolean verified;
 
     public User(String name, String email, String password, String role, String city, boolean verified, String phone) {
         this.name = name;
@@ -18,4 +31,53 @@ public class User {
         this.role = role;
     }
 
+    // setters:
+    public void setId(long id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public void setVerified(boolean verified) {
+        this.verified = verified;
+    }
+
+    //getters:
+    public long getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getPhone(){ return phone; }
+    public String getPassword() {
+        return password;
+    }
+    public String getRole() {
+        return role;
+    }
+    public String getCity() {
+        return city;
+    }
+    public boolean isVerified() {
+        return verified;
+    }
 }
